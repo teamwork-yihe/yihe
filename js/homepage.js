e1b7ff3383669d8dcc2b3ce51f841628d7dbc753
phoenixgelan <phoenixgelan@qq.com>
2016-08-23 20:28:15 +0800

    完成首页右侧滚动条

--- js/homepage.js
+++ js/homepage.js
@@ -1,4 +1,5 @@
 $(function(){
+    // 每一项的图形滑动
     $(document).on("mouseover",".common .titles li",function(i,v){
 	var li = $(this);
 	$(this).parent().find("li").each(function(i,v){
@@ -13,6 +14,36 @@ $(function(){
 	    }
 	});
     });
+    // 页面滚动距离监测
+    var pageTimer = setInterval(function(){
+	// console.log($(window).scrollTop());
+	var height = $(window).scrollTop();
+	console.log(height+"--"+$("body").scrollTop());
+	if(height > 1200){
+	    $(".left-slider").css("display","block");
+	}else{
+	    $(".left-slider").css("display","none");
+	}
+    },200);
+    // 建议反馈
+    $(".response-li").click(function  () {
+	$(window).scrollTop(0);
+	$("#d_xianshi").css("display","block");
+	$("#d_tijiao").css("display","block");
+    })
+    $("#no_setting").click(function  () {
+	$("#d_xianshi").css("display","none");
+	$("#d_tijiao").css("display","none");
+    })
+    $('.tijiao').bind('input propertychange', function() {
+	if (200-$(".tijiao").val().length<=0) {
+	    var news=$(".tijiao").val().substring(0,200);
+	    $(".tijiao").val(news);
+	    alert("亲：字数有点多！");
+	    $(".zishu").html(0+"字");
+	    return;
+	}
+	$(".zishu").html((200-$(".tijiao").val().length)+"字");
 
-
+    });
 });
